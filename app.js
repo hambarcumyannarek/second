30f17e57a0e7cef9e7393ec69cb57d884fa9c61f
hambarcumyannarek <narmenia878@gmail.com>
2023-11-04 21:22:55 +0400

    again

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const { createPool } = require('mysql2/promise');
+// const { createPool } = require('mysql2/promise');
 const app = express();
-
-const pool = createPool({
-    host: process.env.HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    user: process.env.USER
-})
+// 
+// const pool = createPool({
+//     host: process.env.HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     user: process.env.USER
+// })
 
 app.get('/', async (req, res) => {
-    const name = await pool.query('select * from users');
-    res.send('hello ' + name[0][0].name);
+    // const name = await pool.query('select * from users'); name[0][0].name
+    res.send('hello Narek');
 })
 
 app.listen(3001, () => {
     console.log('your server is working');
 })
+
+/* DB_NAME=railway
+DB_PASSWORD=aAh4eH636h-gHfHegGaB-42Ecfb5agd2
+HOST=viaduct.proxy.rlwy.net
+PORT=3000
+USER=root */
